4eda148887ca8df89b60a974d231954eb3ec03cd
Vance Clark <02vance.clark@gulllakecs.org>
2021-10-13 09:48:00 -0700

    Add First and Last Name Variables

--- Control Flow.py
+++ Control Flow.py
@@ -1,4 +1,18 @@
 # Developer: Vance Clark
 # Date: 10/11/2021
-# Program: Control Flow
+# Program: ATM Bank Transaction
 
+'''
+This program simulates an ATM utilizing If, Elif, & Else Statements
+Nesting If statements and refresh our Comparison & Logical Operators
+'''
+
+print("Welcome to Cash-R-Us Bank\nLet's take a moment to set up your account.\n")
+
+# Set up account by asking users their names. Using variables
+first_name = input("What is your first name: ")
+last_name = input("What is your last name: ")
+
+print("\nWelcome to Cash-R-Us", first_name, last_name + ", we will now set up a security pin on your account.\n")
+
+# Set up a pin for the users to have a secure account
